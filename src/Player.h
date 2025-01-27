3d7d0dca9c2402b2fb541b0e37e7b0fa8c498f9d
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2025-01-27 20:38:58 +0100

    Player header

---
+++ src/Player.h
@@ -0,0 +1,14 @@
+﻿// Player.h
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "Global.h"
+
+typedef struct {
+    Entity base;
+    float width;
+    float height;
+    int lives;
+} Player;
+
+#endif // PLAYER_H
